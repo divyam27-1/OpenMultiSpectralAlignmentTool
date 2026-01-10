6639fe7fe0623f843011b80033eab7014676547e
Divyam Tiwari <divyam2701@gmail.com>
2026-01-10 22:47:45 +0530

    Updated package script to copy exiftool

--- package.bat
+++ package.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERSION=1.1.3
+set VERSION=1.2.0
 set DIST_NAME=OMSPEC_v%VERSION%
 set DIST_DIR=dist\%DIST_NAME%
 
@@ -26,9 +26,12 @@ robocopy engine %DIST_DIR%\engine /E /R:0 /W:0 /NFL /NDL /NJH /NJS /NP
 echo "do not edit the engine files or python runtime files. if you edit them you are intentionally hampering the stability and complete functionality of the omspec application" > %DIST_DIR%\engine\DO_NOT_TOUCH.txt
 attrib +R %DIST_DIR%\engine\*.py /S
 
-:: Copy the embedded Python interpreter (assuming it's in your project root)
+:: Copy the embedded Python interpreter
 :: We use /E /I to copy directories and subdirectories
 robocopy python_3_11_14 %DIST_DIR%\python_3_11_14 /E /R:0 /W:0 /COPY:DAT /DCOPY:DAT /NFL /NDL /NJH /NJS /NP
+
+:: Copy the embedded Exiftool for Windows
+robocopy "bin\exiftool-13.45_64" "%DIST_DIR%\bin\exiftool-13.45_64" /E /COPY:DAT /R:3 /W:5 /MT:8 /NFL /NDL /NJH /NJS /NP
 
 :: Copy the License
 copy LICENSE.txt %DIST_DIR%
